2abb784953e9cc3db7a6b3fc68c800e2183aaa27
2017uec1581@mnit.ac.in <2017uec1581@mnit.ac.in>
2020-04-21 10:13:18 +0530

    added signup login

--- app.js
+++ app.js
@@ -8,25 +8,59 @@ var LocalStrategy           =  require("passport-local");
 var passportLocalMongoose   =  require("passport-local-mongoose");
 var methodOverride          =  require("method-override");
 
-mongoose.connect("mongodb://localhost:27017/Dailylife", { useNewUrlParser: true , useUnifiedTopology: true  });
+//MODELS:
+var User                    =  require("./models/User");
 
-//Require your routes here:
-var commentRoutes = require("./routes/comments");
+
+
+
+//Require your ROUTES here:
+var commentRoutes   =   require("./routes/comments");
+var authRoutes      =   require("./routes/authorization");      
+
+
+//Database connection
+mongoose.connect("mongodb://localhost:27017/Dailylife", { useNewUrlParser: true , useUnifiedTopology: true  });
+//-----------------*--------------------*-----------------------------------------*-----------------------------------
+
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
+
+//-------------------------------------------------------------------------------------------------------------------
+//PASSPORT CONFIG :
 app.use(methodOverride("_method"));
+app.use(require("express-session")({
+    secret: "My life is mine i decide what to do",
+    resave: false,
+    saveUninitialized: false
+}));  
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());   
+passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req,res,next){
+	res.locals.currentUser= req.user;
+	next();
+});
 
-//use your Routes here for now i'm making dummy landing page:
+//==========================================================================================================
+//ROUTES: use your Routes here for now i'm making dummy landing page:
 app.get("/",function(req,res){
     res.render("landing");
 });
 
 app.use("/comments",commentRoutes);
+app.use(authRoutes);
 
+
+
+
+//SERVER
 var port = process.env.PORT || 3000;
 app.listen(port,function(){
     console.log("Server started");
