7630886d6bca2d87a7709ef2e8618a09e738c664
2017uec1581@mnit.ac.in <2017uec1581@mnit.ac.in>
2020-04-29 17:38:53 +0530

    changed user authentication(not storing pass)

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var app                     =  express();
 var mongoose                =  require("mongoose");
 var bodyparser              =  require("body-parser");
 var passport                =  require("passport");
+var passportlocal           =  require("passport-local");
 var LocalStrategy           =  require("passport-local").Strategy;
 var passportLocalMongoose   =  require("passport-local-mongoose");
 var methodOverride          =  require("method-override");
@@ -13,11 +14,11 @@ var nodemailer              =  require("nodemailer");
 var crypto                  =  require("crypto");
 var cookieParser            =  require("cookie-parser");
 var flash                   =  require("connect-flash");
-var Journal 				=  require("./models/journal");
+
 
 //MODELS:
 var User                    =  require("./models/User");
-
+var Journal 				=  require("./models/journal");
 
 
 
@@ -34,27 +35,30 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 //Database connection
-mongoose.connect("mongodb://localhost:27017/Dailylife", { useNewUrlParser: true , useUnifiedTopology: true  });
+mongoose.connect("mongodb://localhost:27017/Dailylife2", { useNewUrlParser: true , useUnifiedTopology: true  });
 //-----------------*--------------------*-----------------------------------------*-----------------------------------
 
 
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
-app.use(bodyparser.urlencoded({extended: true}));
-app.use(bodyparser.json());
 app.use(flash());
 
 //-------------------------------------------------------------------------------------------------------------------
 //PASSPORT CONFIG :
 app.use(methodOverride("_method"));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(require("express-session")({
     secret: "My life is mine i will decide what to do",
     resave: false,
     saveUninitialized: true
-}));  
+})); 
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());   
+passport.deserializeUser(User.deserializeUser()); 
 
 // passport.use(new LocalStrategy(
 //     function(username, password, done) {
@@ -70,14 +74,12 @@ app.use(require("express-session")({
 //       });
 //     }
 //   ));
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());   
-passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req,res,next){
-	res.locals.currentUser= req.user;
-	next();
-});
+
+// app.use(function(req,res,next){
+// 	res.locals.currentUser= req.user;
+// 	next();
+// });
 
 //==========================================================================================================
 //ROUTES: use your Routes here for now i'm making dummy landing page:
